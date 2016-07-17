99609eb9ab57d358e279eececa9307ac3a498729
Stefan <slb5418@gmail.com>
2016-07-17 08:47:06 -0400

    fully implemented

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,5 +1,6 @@
 require 'jumpstart_auth'
 require 'bitly'
+require 'klout'
 
 class MicroBlogger
   attr_reader :client
@@ -7,6 +8,8 @@ class MicroBlogger
   def initialize
     puts "Initializing..."
     @client = JumpstartAuth.twitter
+    Bitly.use_api_version_3
+    Klout.api_key = 'xu9ztgnacmjx3bu82warbr3h'
   end
 
   def tweet(message)
@@ -43,22 +46,33 @@ class MicroBlogger
   end
 
   def everyones_last_tweet()
-    friends = @client.friends.collect {|friend| @client.user(friend)}
-    friends.sort_by{|friend| friend.screen_name.downcase}
+    friends = friends_list().sort_by{|friend| friend.screen_name.downcase}
     friends.each do |friend|
       timestamp = friend.status.created_at.strftime("%A, %b %d")
-      puts "#{friend.screen_name} last said this on #{timestamp}:"
+      puts "\n#{friend.screen_name} last said this on #{timestamp}:"
       puts "Last message: #{friend.status.text}"
-      puts ""
     end
   end
 
+  def friends_list()
+    friends = @client.friends.collect{|friend| @client.user(friend)}
+  end
+
   def shorten(original_url)
-    Bitly.use_api_version_3
     bitley = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
     shortened = bitley.shorten(original_url).short_url
     puts shortened
     return shortened
+  end
+
+  def klout_score()
+    friends = friends_list()
+    friends.each do |friend|
+      puts "\nKlout score of #{friend.screen_name}:"
+      identity = Klout::Identity.find_by_screen_name(friend.screen_name)
+      user = Klout::User.new(identity.id)
+      puts user.score.score
+    end
   end
 
   def run
@@ -75,12 +89,12 @@ class MicroBlogger
          when 'spam' then spam_my_followers(parts[1..-1].join(" "))
          when 'elt' then everyones_last_tweet()
          when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
+         when 'klout' then klout_score()
          else
            puts "Sorry, I don't know how to #{command}"
       end
     end
   end
-
 
 end
 
