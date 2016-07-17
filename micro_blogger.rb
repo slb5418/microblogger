23d28c3e5adb9e943cf8ef86d9d443079caed38d
Stefan <slb5418@gmail.com>
2016-07-17 08:27:39 -0400

    elt, turl options implemented

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,4 +1,5 @@
 require 'jumpstart_auth'
+require 'bitly'
 
 class MicroBlogger
   attr_reader :client
@@ -11,9 +12,9 @@ class MicroBlogger
   def tweet(message)
     warning = "Warning! The tweet is longer than 140 characters!"
     if message.length() <= 140
-    	@client.update(message)
+      @client.update(message)
     else
-    	puts warning
+      puts warning
     end
   end
 
@@ -33,23 +34,31 @@ class MicroBlogger
   def spam_my_followers(message)
     followers = followers_list()
     followers.each do |follower|
-    	dm(follower, message)
+      dm(follower, message)
     end
   end
 
   def followers_list()
-  	screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
   end
 
   def everyones_last_tweet()
-  	friends = @client.friends
-  	friends.sort_by{|friend| friend.screen_name.downcase}
-  	friends.each do |friend|
-  	  timestamp = friend.status.created_at.strftime("%A, %b %d")
-  	  puts "Screen name: #{friend.screen_name}"
-  	  puts "Last message: #{friend.status.text}"
-  	  puts ""
-  	end
+    friends = @client.friends.collect {|friend| @client.user(friend)}
+    friends.sort_by{|friend| friend.screen_name.downcase}
+    friends.each do |friend|
+      timestamp = friend.status.created_at.strftime("%A, %b %d")
+      puts "#{friend.screen_name} last said this on #{timestamp}:"
+      puts "Last message: #{friend.status.text}"
+      puts ""
+    end
+  end
+
+  def shorten(original_url)
+    Bitly.use_api_version_3
+    bitley = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    shortened = bitley.shorten(original_url).short_url
+    puts shortened
+    return shortened
   end
 
   def run
@@ -65,13 +74,12 @@ class MicroBlogger
          when 'dm' then dm(parts[1], parts[2..-1].join(" "))
          when 'spam' then spam_my_followers(parts[1..-1].join(" "))
          when 'elt' then everyones_last_tweet()
+         when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
          else
            puts "Sorry, I don't know how to #{command}"
       end
     end
   end
-
-
 
 
 end
